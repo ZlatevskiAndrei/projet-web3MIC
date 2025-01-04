28df1895db85644ebd875b3afacc514fd2c8b0d2
Carole Anh <caroleanh04@gmail.com>
2025-01-05 00:53:41 +0100

    demande userName + correction noms fichiers html dans les nav

---
+++ js/gamerules.js
@@ -0,0 +1,14 @@
+const userName = document.getElementById("userName")
+const enterButton = document.getElementById("enterButton")
+
+enterButton.addEventListener("click",nameComparison)
+
+function nameComparison() {
+    if(userName.value==="Santa Claus" || userName.value==="santa claus" || userName.value==="Santa claus"|| userName.value==="santa Claus"||userName.value==="Santa Claus " || userName.value==="santa claus " || userName.value==="Santa claus "|| userName.value==="santa Claus " ){
+        alert("Well played ! ")
+    }
+    else {
+        alert(userName+", you didn't pay enough attention to the story, you are Santa Claus ! How did you forget that ?")
+    }
+    userName.value=""
+}
